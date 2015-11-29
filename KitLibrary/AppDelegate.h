94ec9f3cd8579d2535bc2aa0ae40382c13b5bbc9
qinfei <qinfei@mogujie.com>
2015-11-29 14:14:06 +0800

    Initial Commit

---
+++ KitLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KitLibrary
+//
+//  Created by qinfei on 11/29/15.
+//  Copyright © 2015 zjut.whw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KitLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KitLibrary
+//
+//  Created by qinfei on 11/29/15.
+//  Copyright © 2015 zjut.whw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
