ba01ad4a6cf621a35dc5f2469a4b1537bd3acd2f
qinfei <qinfei@mogujie.com>
2015-11-29 15:08:16 +0800

    Add Header

---
+++ Header/BaseKitLibrary.h
@@ -0,0 +1,15 @@
+//
+//  BaseKitLibrary.h
+//  KitLibrary
+//
+//  Created by qinfei on 11/29/15.
+//  Copyright © 2015 zjut.whw. All rights reserved.
+//
+
+#ifndef BaseKitLibrary_h
+#define BaseKitLibrary_h
+
+#import "BaseViewController.h"
+
+
+#endif /* BaseKitLibrary_h */
--- KitLibrary/AppDelegate.h
+++ KitLibrary/AppDelegate.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+#import "BaseKitLibrary.h"
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
